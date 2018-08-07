2402924f11f577ac141c626db388e67d96ec2cac
Dima Bellavin <izume@inbox.ru>
2018-08-07 22:21:22 +0300

    Начинаем программировать

--- js/main.js
+++ js/main.js
@@ -32,41 +32,30 @@ document.addEventListener(`keydown`, (evt) => {
   }
 });
 
-
-const createArrow = (textContent) => {
-  const arrow = document.createElement(`button`);
-  arrow.classList.add(`arrows__btn`);
-  arrow.textContent = textContent;
-  // arrow.style.background = `none`;
-  arrow.style.background = `white`;
-  arrow.style.border = `2px solid black`;
-  arrow.style.padding = `5px 20px`;
-
-  return arrow;
-};
-
-
-const createArrowsWrap = (parentNode) => {
-  const wrap = document.createElement(`div`);
-  wrap.classList.add(`arrows__wrap`);
-
-  wrap.style.position = `absolute`;
-  wrap.style.top = `135px`;
-  wrap.style.left = `50%`;
-  wrap.style.marginLeft = `-56px`;
-
-  parentNode.appendChild(wrap);
-
-  return wrap;
-};
+const arrowsHTMLString = `<div class="arrows__wrap">
+  <style>
+    .arrows__wrap {
+      position: absolute;
+      top: 135px;
+      left: 50%;
+      margin-left: -56px;
+    }
+    .arrows__btn {
+      background: none;
+      border: 2px solid black;
+      padding: 5px 20px;
+    }
+  </style>
+  <button class="arrows__btn"><-</button>
+  <button class="arrows__btn">-></button>
+</div>`;
 
 const main = document.querySelector(`main.app`);
-const arrowsWrap = createArrowsWrap(main);
+main.insertAdjacentHTML(`beforeEnd`, arrowsHTMLString);
 
-const leftArrow = createArrow(`<-`);
-const rightArrow = createArrow(`->`);
-arrowsWrap.appendChild(leftArrow);
-arrowsWrap.appendChild(rightArrow);
+const arrowsWrap = main.querySelector(`.arrows__wrap`);
+const leftArrow = arrowsWrap.querySelector(`.arrows__btn:first-of-type`);
+const rightArrow = arrowsWrap.querySelector(`.arrows__btn:last-of-type`);
 
 leftArrow.addEventListener(`click`, () => {
   select(current - 1);
